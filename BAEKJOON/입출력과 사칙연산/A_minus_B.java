34f507d020b618b682ab025c20ce9226152c13d2
lindarong29 <dahye0209@naver.com>
2021-06-16 10:01:53 +0900

    입출력과 사칙연산

---
+++ BAEKJOON/입출력과 사칙연산/A_minus_B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class A_minus_B {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+		
+		System.out.println(A-B);
+	}
+	
+}
---
+++ BAEKJOON/입출력과 사칙연산/Cat.java
@@ -0,0 +1,19 @@
+/*
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+ */
+
+public class Cat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		// '\'는 앞에 '\'를 하나 더 붙여서 기능 취소.
+	}
+
+}
